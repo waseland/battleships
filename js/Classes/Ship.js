f07b9271e435b798fa6ac7b2a16b13c91e4772e0
PatrickDFFY <pjmduffy@student.avans.nl>
2015-06-18 01:49:15 +0200

    Ships & Ship Styles

--- js/Classes/Ship.js
+++ js/Classes/Ship.js
@@ -2,10 +2,33 @@ $(document).ready(function Ship(){
 
 	var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.InBqbWR1ZmZ5QGF2YW5zLm5sIg.3_74qaKqZl4lhjCAXyzZknR4OPpnTk6OzwdYaHN9OJw";
 
+	function selectShip(id){
+		switch(id) {
+		    case 0:
+		        $('#ship-image-container').html('<img src="img/patrolboat.png">');
+		        break;
+		    case 1:
+		    	$('#ship-image-container').html('<img src="img/destroyer.png">');
+		        break;
+		    case 2:
+		    	$('#ship-image-container').html('<img src="img/submarine.png">');
+		        break;
+		    case 3:
+		    	$('#ship-image-container').html('<img src="img/battleship.png">');
+		        break;
+		    case 4:
+		    	$('#ship-image-container').html('<img src="img/aircraftcarrier.png">');
+		        break;
+		    default:
+		    	break;
+		}
+	}
+
 	$.get('https://zeeslagavans.herokuapp.com/ships?token=' + token,
 		function(data){
 			for(var i = 0; i < data.length; i++){
-				$('#ships').append('<li class="list-group-item" draggable="true" ondragstart="selectShip('+ data[i]._id + ')"><button class="btn btn-primary btn-xs pull-left"><i class="fa fa-ship"></i></button><span class="badge">' + data[i].length + '</span><a href="#">' + data[i].name + '</a></li>');
+				$('#ships').append('<li class="list-group-item" draggable="true" ondragstart="selectShip('+ data[i]._id + ')"><button id="orientation" class="btn btn-primary btn-xs pull-left"><i class="fa fa-ship"></i></button><span class="badge">' + data[i].length + '</span><a href="#">' + data[i].name + '</a></li>');
 			}
 		});
 });
+
